72b6e508b67698008efaf1f5fe6420915ff0c0f7
RajlaxmiMeshram <rajlaxmimeshram024@gmail.com>
2022-12-11 16:44:32 +0530

    u

--- TicTok/Form1.cs
+++ TicTok/Form1.cs
@@ -6,10 +6,10 @@ namespace TicTok
     public partial class Form1 : Form
     {
         public int tea, coffee,sshake,cshake=0;
-        public int cprice=20;
-        public int tprice=10;
-        public int sprice = 130;
-        public int coprice = 110;
+        public int cprice;
+        public int tprice;
+        public int sprice;
+        public int coprice;
 
         public Form1()
         {
@@ -34,25 +34,29 @@ namespace TicTok
 
         private void button1_Click(object sender, EventArgs e)
         {
-            tea = tea + 1;
-           tprice = tprice + tea;
-            richTextBox1.AppendText("Tea    "+tea.ToString()+"   "+tprice.ToString());
+            tea = tea+1
+           tprice = 10 * tea;
+            richTextBox1.AppendText("Tea    "+tea.ToString()+"   "+tprice.ToString()+ "\n");
+       
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-           cshake= cshake + 1;
-            cprice = cprice + cshake;
+           cshake= cshake+1;
+            coprice = 150 + cshake;
+            richTextBox1.AppendText("Chocolate Shake    " + cshake.ToString() + "   " + coprice.ToString() + "\n");
         }
         private void button3_Click(object sender, EventArgs e)
         {
-            coffee = coffee + 1;
-            cprice = cprice + coffee;
+            coffee = coffee+1;
+            cprice = 50 + coffee;
+            richTextBox1.AppendText("Coffee   " + coffee.ToString() + "   " + cprice.ToString() + "\n");
         }
         private void button4_Click(object sender, EventArgs e)
         {
             sshake = sshake + 1;
-            sprice = sprice + sshake;
+            sprice = 180 + sshake;
+            richTextBox1.AppendText("Stawberry Shake   " + sshake.ToString() + "   " + sprice.ToString() + "\n");
         }
 
         private void Form1_Load(object sender, EventArgs e)
